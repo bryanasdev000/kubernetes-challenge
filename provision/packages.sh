d42c385263fcd1ffd4d4ad4fdc3827dea213e549
Hector Vido <hector_vido@yahoo.com.br>
2020-08-21 19:22:02 -0300

    Modified docker packages from docker repositories to debian

--- provision/packages.sh
+++ provision/packages.sh
@@ -5,9 +5,8 @@ apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-prop
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' > /etc/apt/sources.list.d/kubernetes.list
-echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io kubelet kubeadm kubectl
+apt-get install -y docker.io kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
 
 echo '{
